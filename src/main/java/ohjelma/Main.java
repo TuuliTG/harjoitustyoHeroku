4317ca3f42e85c7ae67738f7e177baeda2c6fd94
Toivanen-Gripentrog Tuuli <tgtuuli@da5-cs-b221-13.pc.helsinki.fi>
2018-10-12 14:10:57 +0300

    testi toimii

--- src/main/java/ohjelma/Main.java
+++ src/main/java/ohjelma/Main.java
@@ -1,12 +1,29 @@
 package ohjelma;
 
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 
 public class Main {
     public static void main(String[] args) {
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
+        
+        System.out.println("Hello world");
+    
+    
+    Spark.get("*", (req, res) -> {
+       HashMap map = new HashMap();
+       map.put("teksti", "hello world");
+        return  new ModelAndView(map, "etusivu");
+    }, new ThymeleafTemplateEngine());
+    
     }
     
 }
