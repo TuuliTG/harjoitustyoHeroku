5fd8a5938f4d14b76676ef3ddd40ed348f86e1c5
Tuuli Toivanen-Gripentrog <tuuli.k.toivanen@gmail.com>
2018-10-12 20:30:43 +0300

    pääohjelman muutokset

--- src/main/java/ohjelma/Main.java
+++ src/main/java/ohjelma/Main.java
@@ -1,8 +1,11 @@
 package ohjelma;
 
 
+import dao.KysymysDao;
+import dao.VastausDao;
 import database.Database;
-import java.sql.Connection;
+import domain.Kysymys;
+import domain.Vastaus;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,18 +21,66 @@ public class Main {
         }
         
         Database database = new Database();
-        Connection conn = database.getConnection();
-        
+        KysymysDao kysymykset = new KysymysDao(database);
+        VastausDao vastaukset = new VastausDao(database);
         
         System.out.println("Hello world");
     
     
-    Spark.get("*", (req, res) -> {
-       HashMap map = new HashMap();
-       map.put("teksti", "hello world");
-        return  new ModelAndView(map, "etusivu");
-    }, new ThymeleafTemplateEngine());
-    
+     Spark.get("/kysymykset", (req, res) -> {
+            
+            HashMap map  = new HashMap<>();
+            map.put("kokeiluKurssi", kysymykset.findAll());
+            
+            return new ModelAndView(map, "etusivu");
+        }, new ThymeleafTemplateEngine());
+        
+        Spark.post("/kysymykset", (req, res) -> {
+            
+            String kurssi = req.queryParams("kurssin nimi");
+            String aihe = req.queryParams("aihe");
+            String kysymysteksti = req.queryParams("teksti");
+            
+            Kysymys uusiKysymys = new Kysymys(kurssi, aihe, kysymysteksti);
+            kysymykset.saveOrUpdate(uusiKysymys);
+            
+            res.redirect("/kysymykset");
+            return "";
+        });
+        
+        Spark.get("/kysymys/:id", (req, res) -> {
+            HashMap map = new HashMap<>();
+            Integer kysymysId = Integer.parseInt(req.params(":id"));
+            List<Vastaus> vastauksetKysymykseen = vastaukset.kysymyksenKaikkiVastaukset(kysymysId);
+            
+            map.put("kysymys", kysymykset.findOne(kysymysId));
+            map.put("vastaukset", vastauksetKysymykseen);
+            
+            return new ModelAndView(map, "vastausvaihtoehdot");
+        }, new ThymeleafTemplateEngine());
+        
+        Spark.post("/poistakysymys/:id", (req, res) -> {
+            Integer kysymysId = Integer.parseInt(req.params(":id"));
+            res.redirect("/kysymykset");
+            return "";
+        });
+        
+        Spark.post("/lisaakysymys/:kysymysid", (req, res) -> {
+            boolean onOikein = false;
+            Integer kysymysId = Integer.parseInt(req.params(":kysymysid"));
+            System.out.println(kysymysId);
+            String checkboxValues = req.queryParams("onOikein");
+            if(checkboxValues != null) {
+                onOikein = true;
+            }
+            String vastausteksti = req.queryParams("vastausteksti");
+            System.out.println(vastausteksti);
+            System.out.println(onOikein);
+            Vastaus vastaus = new Vastaus(kysymysId, vastausteksti, onOikein);
+            vastaukset.saveOrUpdate(vastaus);
+            res.redirect("/kysymykset");
+            return "";
+        });
     }
     
 }
